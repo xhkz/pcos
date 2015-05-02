7dd6614395e77037383dec7a0420cdb064a17e81
pOOOOOr <au.huangxin@gmail.com>
2015-05-03 06:39:23 +1000

    remove files

--- pcos/config.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'Xin Huang'
-
-DEBUG = True
-TESTING = True
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://xinhuang:1@115.146.93.76/pcos'
-SECRET_KEY = '\x02A\xc9\xaf\xacQ\xbd\x9c\xb576kh\xda\xa2\xccaDcS\x8cp"\x87'
--- run.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'Xin Huang'
-
-if __name__ == '__main__':
-    from pcos import app
-    app.run()
